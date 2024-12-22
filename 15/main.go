49e39a20ce473de0c4df5c12ed4af7bb577ef23a
Slavik <caitlynl@student.21-school.ru>
2024-12-22 23:37:30 +0300

    lvl1 finish

--- 15/main.go
+++ 15/main.go
@@ -5,10 +5,21 @@
 package main
 
 import (
-	"strings"
+  "strings"
+  "math/rand"
 )
 
 var justString string
+
+func createHugeString(n int) string{
+  letters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  result := make([]byte, n)
+ 
+  for i := 0; i < n; i++ {
+   result[i] = letters[rand.Intn(len(letters))]
+  }
+  return string(result)
+}
 
 func someFunc() {
   v := createHugeString(1 << 10)
